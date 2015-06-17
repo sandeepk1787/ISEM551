55cfd724520ea944400efde1d037c8ba290b06d1
unknown <SKesireddy@my.harrisburgu.edu>
2015-06-17 05:48:40 -0400

    Assignment 1 -- Part 2,3,4,5

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    alert("My Email Address is SKesireddy@my.harrisburgu.edu");
+}
+
+function resetFunction() {
+    document.getElementById("ContactUsForm").reset();
+}
+
+
+function submitFunction() {
+    alert("Your Enquiry has been submitted succesfully");
+	resetFunction();
+}
+
